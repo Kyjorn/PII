35acf4073ed64c3040e18fa7215d39dfc960e660
Kyjorn <hendonmshane@gmail.com>
2025-03-21 13:28:24 -0600

    Add user prompts for personal information input in PII.cpp

--- .vscode/PII.cpp
+++ .vscode/PII.cpp
@@ -4,11 +4,28 @@ using namespace std;
 int main () {
 
     //variables
-    string firstName = "Jack";
-    string lastName = "Skelly";
-    string streetAddress = "666 Down Unda ln.";
-    string city = "Halloween Town";
-    int zipCode = 123456;
+    string firstName;
+    string lastName;
+    string streetAddress;
+    string city;
+    int zipCode;
+
+    //prompts for user
+        cout << "Enter your first name: ";
+        cin >> firstName;
+        
+        cout << "Enter your last name: ";
+        cin >> lastName;
+
+        cin.ignore();
+        cout << "Enter your street address: ";
+        getline (cin, streetAddress);
+        
+        cout << "Enter your city: ";
+        getline (cin, city);
+
+        cout << "Enter your zip code: ";
+        cin >> zipCode;
 
     //cout
     cout << firstName << endl;
