ba7dd62782f5d992f39e3612f782712251faceed
Kyjorn <hendonmshane@gmail.com>
2025-03-21 13:05:57 -0600

    Initialize personal information variables in PII.cpp with sample data

--- .vscode/PII.cpp
+++ .vscode/PII.cpp
@@ -4,12 +4,16 @@ using namespace std;
 int main () {
 
     //variables
-    string firstName;
-    string lastName;
-    string streetAddress;
-    string city;
-    int zipCode;
+    string firstName = "Jack";
+    string lastName = "Skelly";
+    string streetAddress = "666 Down Unda ln.";
+    string city = "Halloween Town";
+    int zipCode = 123456;
 
-    
+    cout << firstName << endl;
+    cout << lastName << endl;
+    cout << streetAddress << endl;
+    cout << city << endl;
+    cout << zipCode << endl;
 return 0;
 }
