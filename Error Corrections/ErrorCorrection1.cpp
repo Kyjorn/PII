e5242e14e6a1645debbf804a4f783a09133ae92e
Kyjorn <hendonmshane@gmail.com>
2025-03-21 13:47:31 -0600

    Fix syntax errors and improve comments in ErrorCorrection1.cpp

--- Error Corrections/ErrorCorrection1.cpp
+++ Error Corrections/ErrorCorrection1.cpp
@@ -1,15 +1,20 @@
-/* Simple Program with a few Errors for Correction
-Please be sure to correct all outlined errors.
+// Simple Program with a few Errors for Correction
+//Please be sure to correct all outlined errors.
+
 #include<iostream>
 #include<conio.h>
-Standard namespace declaration
+
+//Standard namespace declaration
 using namespace std;
-Main Function
-int main()
-{
+
+//Main Function
+int main() {
+
 //Standard Ouput Statement
 cout<<"Welcome to this C++ Program"<<endl;
-cout<<I have corrected all errors for this program. <<endl;
+cout<<"I have corrected all errors for this program." <<endl;
+
 // Wait For Output Screen
 //Main Function return Statement
+return 0;
 }
