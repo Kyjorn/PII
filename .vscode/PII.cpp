6ce185bba86d9e1f8ec67d2008e1141d5d199d6b
Kyjorn <hendonmshane@gmail.com>
2025-03-21 13:02:18 -0600

    Add variable declarations for personal information in PII.cpp

--- .vscode/PII.cpp
+++ .vscode/PII.cpp
@@ -3,5 +3,13 @@ using namespace std;
 
 int main () {
 
+    //variables
+    string firstName;
+    string lastName;
+    string streetAddress;
+    string city;
+    int zipCode;
+
+    
 return 0;
 }
